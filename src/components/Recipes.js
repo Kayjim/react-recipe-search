b205c3220507b8489b756a905fc28f5e5771edfb
Jimmy Dang <jdang@empoweredbenefits.com>
2018-10-09 14:54:31 -0400

    React Recipe Search v1.0

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,15 +1,52 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 const Recipes = props => (
-  <div>
-    {props.recipes.map(recipe => {
-      return (
-        <div key={recipe.recipe_id}>
-          <img src={recipe.img_url} alt={recipe.title} />
-          <p> {recipe.title}</p>}
-        </div>
-      );
-    })}
+  <div className="container">
+    <div className="row">
+      {props.recipes.map(recipe => {
+        return (
+          <div
+            key={recipe.title}
+            className="col-md-4"
+            style={{ marginBottom: "2rem" }}
+          >
+            <div className="recipes__box">
+              <div>
+                <img
+                  className="recipe__box-img"
+                  src={recipe.image_url}
+                  alt={recipe.title}
+                />
+                <div className="recipe-text">
+                  <h5 className="recipes__title">
+                    {recipe.title.length < 20
+                      ? `${recipe.title}`
+                      : `${recipe.title.substring(0, 25)}...`}
+                  </h5>
+                  <p className="recipes_subtitle">
+                    {" "}
+                    Publisher: <span>{recipe.publisher}</span>{" "}
+                  </p>
+                </div>
+                <button className="recipe_buttons">
+                  {" "}
+                  <Link
+                    to={{
+                      pathname: `/recipe/${recipe.recipe_id}`,
+                      state: { recipe: recipe.title }
+                    }}
+                  >
+                    View Recipe
+                  </Link>{" "}
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   </div>
 );
 
